3558eb4e0de11ff124a95a2e9a6cda2bfe8b4ef9
jordybsk <dylan.bossoku@jobtrek.ch>
2023-12-04 10:46:55 +0100

    update: update add semester and we can add grades when we create a new semester

--- src/main.js
+++ src/main.js
@@ -1,43 +1,64 @@
 const button = document.querySelector("#button");
-const input = document.querySelector("#sem1");
-const divGrades = document.querySelector("#grades");
+const input = document.querySelector(".sem1");
 const semesterButton = document.querySelector("#add-semester");
 let count = 2;
+const divGrades = document.querySelector(".grades");
 
-button.addEventListener("click", () => {
-  const svgOrange = document
-    .getElementById("svg-orange")
-    .content.querySelector("svg");
-  const svgRed = document
-    .getElementById("svg-red")
-    .content.querySelector("svg");
-  const svgGreen = document
-    .getElementById("svg-green")
-    .content.querySelector("svg");
+semesterButton.addEventListener("click", (event) => {
+  count++;
+  const allSemester = document.querySelector("#all-semester");
+  const newSemestre = document
+    .querySelector("#semester-template")
+    .content.cloneNode(true);
+  const buttonGrades = newSemestre.querySelector("button");
+  const inputSemester = newSemestre.querySelector("input");
+  let divGrades = newSemestre.querySelector(".grades");
 
-  if (input.value <= 6 && input.value % 0.5 === 0 && input.value > 0.5) {
-    const newGrades = document.createElement("span");
-    newGrades.className =
-      "inline-flex items-center gap-x-1.5 rounded-md px-2 py-2 text-sm font-medium text-gray-900 ring-1 ring-inset ring-gray-200";
+  if (count <= 8) {
+    allSemester.appendChild(newSemestre);
+    const semester = allSemester.lastElementChild;
+    semester.querySelector("dt").innerText = "Semester " + count;
 
-    let newSvg = document.createElement("svg");
+    buttonGrades.addEventListener("click", () => {
+      const svgOrange = document
+        .getElementById("svg-orange")
+        .content.querySelector("svg");
+      const svgRed = document
+        .getElementById("svg-red")
+        .content.querySelector("svg");
+      const svgGreen = document
+        .getElementById("svg-green")
+        .content.querySelector("svg");
 
-    if (input.value > 4) {
-      newSvg = svgGreen.cloneNode(true);
-    } else if (input.value < 4) {
-      newSvg = svgRed.cloneNode(true);
-    } else {
-      newSvg = svgOrange.cloneNode(true);
-    }
+      if (
+        inputSemester.value <= 6 &&
+        inputSemester.value % 0.5 === 0 &&
+        inputSemester.value > 0.5
+      ) {
+        const newGrades = document.createElement("span");
+        newGrades.className =
+          "inline-flex items-center gap-x-1.5 rounded-md px-2 py-2 text-sm font-medium text-gray-900 ring-1 ring-inset ring-gray-200";
 
-    newGrades.appendChild(newSvg);
-    newGrades.appendChild(document.createTextNode(input.value));
+        let newSvg = document.createElement("svg");
 
-    if (divGrades) {
-      divGrades.appendChild(newGrades);
-    }
+        if (inputSemester.value > 4) {
+          newSvg = svgGreen.cloneNode(true);
+        } else if (inputSemester.value < 4) {
+          newSvg = svgRed.cloneNode(true);
+        } else {
+          newSvg = svgOrange.cloneNode(true);
+        }
+
+        newGrades.appendChild(newSvg);
+        newGrades.appendChild(document.createTextNode(inputSemester.value));
+
+        if (divGrades) {
+          divGrades.appendChild(newGrades);
+        }
+      }
+      inputSemester.value = "";
+    });
   }
-  input.value = "";
 });
 
 input.addEventListener("focus", (event) => {
@@ -60,18 +81,4 @@ input.addEventListener("blur", (event) => {
 
   svgClassList.classList.remove("text-blue-400");
   svgClassList.classList.add("text-gray-400");
-});
-
-semesterButton.addEventListener("click", (event) => {
-  count++;
-  const allSemester = document.querySelector("#all-semester");
-  const newSemestre = document
-    .querySelector("#semester-template")
-    .content.cloneNode(true);
-
-  if (count <= 8) {
-    allSemester.appendChild(newSemestre);
-    const semester = allSemester.lastElementChild;
-    semester.querySelector("dt").innerText = "Semester " + count;
-  }
 });
